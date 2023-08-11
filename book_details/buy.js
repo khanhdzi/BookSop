4aca48e1cb491c48d5d63b6ce9a8c9e54fc23222
khanhdzi <131856268+khanhdzi@users.noreply.github.com>
2023-08-11 23:49:38 +0700

    All Files

---
+++ book_details/buy.js
@@ -0,0 +1,10 @@
+// RMIT University Vietnam
+// Course: COSC2430 Web Programming
+// Semester: 2023A
+// Assessment: Assignment 1
+// Author: Nguyen Thach Khanh Dzi
+// ID: s3980883
+// Acknowledgement: Acknowledge the resources that you use here. 
+function myFunction() {
+    alert("Thank you for choosing BookSop <3");
+  }
